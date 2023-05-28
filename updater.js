443986b08ea9cbb797dc14c6f4b9794c1fa81af9
colafly <aauddrey@gmail.com>
2023-05-28 09:04:02 +0800

    delete and revoke token

--- updater.js
+++ updater.js
@@ -5,13 +5,6 @@ autoUpdater.logger = require("electron-log")
 autoUpdater.logger.transports.file.level = "info"
 
 module.exports = () => {
-    autoUpdater.setFeedURL({
-        provider: 'github',
-        repo: 'repo',
-        owner: 'owner',
-        private: true,
-        token: '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'
-    })
     autoUpdater.checkForUpdates().then(info => {
         if (info.updateAvailable) {
             autoUpdater.downloadUpdate().then(
